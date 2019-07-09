cf5c2ab2dc16b4b43142ad0ba22317278cad1428
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-07-09 16:01:08 -0700

    funcationality is working, need to add styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from "./components/DisplayComponents/Display";
 import Logo from "./components/DisplayComponents/Logo";
 
 function App() {
-  // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
-  // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
-  // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
-  // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
-  // Don't forget to pass the functions (and any additional data needed) to the components as props
+  const [display, setDisplay] = useState("");
+  const [holdingState, setHoldingState] = useState("");
+  const [operatorMath, setOperatorMath] = useState("");
+
+  const numberHandler = number => {
+    setDisplay(parseInt(display + number));
+  };
+
+  const specialHandler = special => {
+    if (special === "C") {
+      setDisplay("");
+      setHoldingState("");
+    }
+  };
+
+  const operatorHandler = (char, value) => {
+    if (char === "+" || "-" || "x" || "/") {
+      setHoldingState(display);
+      setOperatorMath(value);
+      setDisplay("");
+    }
+    if (char === "=") {
+      setDisplay(eval(`holdingState ${operatorMath} display`));
+      setHoldingState("");
+      setOperatorMath("");
+    }
+  };
 
   return (
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display display={display} />
+        <Specials specialHandler={specialHandler} />
+        <Numbers numberHandler={numberHandler} />
+        <Operators operatorHandler={operatorHandler} />
       </div>
     </div>
   );
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = props => {
   return (
     <>
+      <button onClick={() => props.operatorHandler(props.char, props.value)}>
+        {props.char}
+      </button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
 };
+
+export default OperatorButton;
